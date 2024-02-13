c40f67196376856f664e3cf36bf171e6a73c3316
Makar Dezhemesov <dezhemesov.work@gmail.com>
2024-02-13 15:44:27 +0300

    Use ceil instead of floor

--- Assets/Scripts/UI/Converters/FloatToIntStringConverter.cs
+++ Assets/Scripts/UI/Converters/FloatToIntStringConverter.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using UnityEngine;
 using UnityMvvmToolkit.Core.Converters.PropertyValueConverters;
 
@@ -6,7 +7,7 @@ namespace TicTacToe.UI.Converters
     public class FloatToIntStringConverter: PropertyValueConverter<float, string>
     {
         public override string Convert(float value)
-            => Mathf.FloorToInt(value + 0.5f).ToString();
+            => Mathf.Ceil(value).ToString(CultureInfo.InvariantCulture);
 
         public override float ConvertBack(string value) 
             => int.TryParse(value, out var num) ? num : default(float);
