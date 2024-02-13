51edaae6f9ef460e769d3bddfb92c162440f2a2e
Makar Dezhemesov <dezhemesov.work@gmail.com>
2024-02-13 21:29:58 +0300

    Small ai tweaks

--- Assets/Scripts/Services/Hint/HintService.cs
+++ Assets/Scripts/Services/Hint/HintService.cs
@@ -1,28 +1,55 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using TicTacToe.Services.GameBoard;
 using TicTacToe.Services.GameBoard.BoardPlayers;
 using TicTacToe.Services.GameBoard.Rules;
+using TicTacToe.Services.Randomizer;
 using UnityEngine;
 
 namespace TicTacToe.Services.Hint
 {
     public class HintService : IHintService
     {
+        private readonly IRandomService _randomService;
         private readonly MiniMax _miniMax;
 
-        public HintService(IGameRules gameRules)
+        public HintService(IGameRules gameRules, IRandomService randomService)
         {
             _miniMax = new MiniMax(gameRules);
+            _randomService = randomService;
         }
 
         public async UniTask<Vector2Int> GetBestMove(GameTile[,] sourceBoard, IPlayer player, IPlayer other)
         {
+            var freeTiles = 0;
+            foreach (var tile in sourceBoard)
+            {
+                if (!tile.IsOccupied)
+                    freeTiles++;
+            }
+
+            if (freeTiles > 9)
+                return GetRandomTile(sourceBoard);
+                
+            
             var board = CloneBoard(sourceBoard);
             var task = Task<Vector2Int>.Factory.StartNew(() => _miniMax.GetBestMove(board, player, other));
             await task.AsUniTask();
             return task.Result;
+        }
+
+        private Vector2Int GetRandomTile(GameTile[,] board)
+        {
+            var freeTiles = new List<GameTile>();
+            foreach (var tile in board)
+            {
+                if (!tile.IsOccupied)
+                    freeTiles.Add(tile);
+            }
+
+            return freeTiles[_randomService.GetInRange(0, freeTiles.Count - 1)].Position;
         }
 
         private static GameTile[,] CloneBoard(GameTile[,] sourceBoard)
