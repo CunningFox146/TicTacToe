d3dfaaa8a71e008d456ee137bc97bc48356b76a4
Makar Dezhemesov <dezhemesov.work@gmail.com>
2024-02-14 15:19:28 +0300

    Refactor test cinfrastructure

--- Assets/Scripts/Tests/Common/Infrastructure/ZenjectPlayModeUnitTestFixture.cs
+++ Assets/Scripts/Tests/Common/Infrastructure/ZenjectPlayModeUnitTestFixture.cs
@@ -1,28 +1,25 @@
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Zenject;
 
 namespace TicTacToe.Tests.Common.Infrastructure
 {
     public class ZenjectPlayModeUnitTestFixture : ZenjectUnitTestFixture
     {
-        protected DiContainer Container { get; private set; }
-        
         [SetUp]
-        public virtual void SetupTestContainer()
+        public override void SetupTestContainer()
         {
+            base.SetupTestContainer();
+            
             var itemsContainer = new GameObject("Test Container");
             SceneManager.MoveGameObjectToScene(itemsContainer, SceneManager.GetActiveScene());
-            
-            Container = GlobalContainer.CreateSubContainer();
             Container.DefaultParent = itemsContainer.transform;
         }
         
         [TearDown]
-        public void ClearTestContainer()
+        public override void ClearTestContainer()
         {
-            Container.UnbindAll();
+            base.ClearTestContainer();
             Object.DestroyImmediate(Container.DefaultParent.gameObject);
         }
     }
--- Assets/Scripts/Tests/Common/Infrastructure/ZenjectUnitTestFixture.cs
+++ Assets/Scripts/Tests/Common/Infrastructure/ZenjectUnitTestFixture.cs
@@ -5,12 +5,27 @@ namespace TicTacToe.Tests.Common.Infrastructure
 {
     public abstract class ZenjectUnitTestFixture
     {
+        
+        protected DiContainer Container { get; private set; }
         protected DiContainer GlobalContainer { get; private set; }
 
         [OneTimeSetUp]
         public virtual void SetupGlobalContainer()
         {
             GlobalContainer = new DiContainer();
+        }
+        
+        [SetUp]
+        public virtual void SetupTestContainer()
+        {
+            Container = GlobalContainer.CreateSubContainer();
+        }
+        
+        
+        [TearDown]
+        public virtual void ClearTestContainer()
+        {
+            Container.UnbindAll();
         }
     }
 }
