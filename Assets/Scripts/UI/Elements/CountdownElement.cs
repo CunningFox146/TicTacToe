351c58968172c0f0f625915ddf2c33e38f21bd14
Makar Dezhemesov <dezhemesov.work@gmail.com>
2024-02-15 03:45:05 +0300

    Project finished

---
+++ Assets/Scripts/UI/Elements/CountdownElement.cs
@@ -0,0 +1,46 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+namespace TicTacToe.UI.Elements
+{
+    public class CountdownElement : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _text;
+        private Sequence _anim;
+        private string _oldText;
+        
+        private RectTransform Target => (RectTransform)_text.transform;
+
+        private void OnDisable()
+        {
+            Target.localEulerAngles = Vector3.zero;
+            Target.localScale = Vector3.one;
+            _anim?.Kill();
+        }
+
+        private void Update()
+        {
+            UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            if (_text.text == _oldText)
+                return;
+            _oldText = _text.text;
+            PlayAnim();
+        }
+
+        private void PlayAnim()
+        {
+            Target.localScale = Vector3.zero;
+
+            _anim = DOTween.Sequence()
+                .Join(Target.DORotate(Vector3.forward * 360f, 0.25f, RotateMode.FastBeyond360))
+                .Join(Target.DOScale(Vector3.one, 0.25f))
+                .Append(Target.DORotate(Vector3.back * 360f, 0.25f, RotateMode.FastBeyond360).SetDelay(0.5f))
+                .Join(Target.DOScale(Vector3.zero, 0.25f));
+        }
+    }
+}
